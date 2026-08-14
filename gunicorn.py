695e926a089649c169f6d9bf8cf3508308febf0b
yingkong078-cmyk <yingkong078@gmail.com>
2026-08-14 17:55:51 +0700

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -1,11 +1,3 @@
-# gunicorn.py - 生产环境启动文件
-
-import sys
-import os
-
-# 添加当前目录到路径
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from web_translator import app
 
 if __name__ == "__main__":
